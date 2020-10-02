6e95c8db9ad52b740b6d4fa5a49544ecc88bb000
TheHong <chcheung.office@gmail.com>
2020-10-02 18:33:18 -0400

    Remove unneeded screen2

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:ivyhack/screens/auth_wrapper.dart';
-import 'package:ivyhack/screens/screen2/screen2.dart';
-
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   '/': (context) => AuthWrapper(),
-  '/page2': (context) => AnotherScreen(),
 };

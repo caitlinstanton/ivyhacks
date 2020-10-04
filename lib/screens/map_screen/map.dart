c6f1461cd91c293498332b519a7a022fc44517a5
TheHong <chcheung.office@gmail.com>
2020-10-04 10:22:22 -0400

    Map works

--- lib/screens/map_screen/map.dart
+++ lib/screens/map_screen/map.dart
@@ -1,3 +1,5 @@
+// Based on https://codelabs.developers.google.com/codelabs/google-maps-in-flutter/#3
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -8,8 +10,27 @@ class MapWidget extends StatefulWidget {
 
 class _MapState extends State<MapWidget> {
   GoogleMapController mapController;
-
   final LatLng _center = const LatLng(45.521563, -122.677433);
+  List<Marker> allMarkers = [
+    Marker(
+      markerId: MarkerId("Seattle"),
+      draggable: false,
+      position: LatLng(47.6, -122.32),
+      infoWindow: InfoWindow(title: "Seattle Avg: 60"),
+    ),
+    Marker(
+      markerId: MarkerId("Portland"),
+      draggable: false,
+      position: LatLng(45.521563, -122.677433),
+      infoWindow: InfoWindow(title: "Portland Avg: 65"),
+    ),
+    Marker(
+      markerId: MarkerId("Vancouver"),
+      draggable: false,
+      position: LatLng(49.25, -123.1),
+      infoWindow: InfoWindow(title: "Vancouver Avg: 22"),
+    )
+  ];
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -19,16 +40,13 @@ class _MapState extends State<MapWidget> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
             target: _center,
             zoom: 11.0,
           ),
+          markers: allMarkers.toSet(),
         ),
       ),
     );

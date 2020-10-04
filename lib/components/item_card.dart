af050e82f512cc86ab57ce41015d2df0a690b963
TheHong <chcheung.office@gmail.com>
2020-10-03 22:45:03 -0400

    Complete settings page

---
+++ lib/components/item_card.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:ivyhack/models/text.dart';
+
+Widget itemCard(String text) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+    child: Container(
+        padding: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+          color: Colors.green[100],
+        ),
+        child: Center(
+          child: SenText(text, fontSize: 20),
+        )),
+  );
+}
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ivyhack/components/item_card.dart';
+import 'package:ivyhack/models/text.dart';
 import 'package:ivyhack/services/auth_service.dart';
 
 class SettingsScreen extends StatefulWidget {
@@ -7,26 +9,57 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
-  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Center(
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/settings_background.png"),
+            fit: BoxFit.cover),
+      ),
+      child: Center(
         child: Column(
           children: [
-            Text(
-              "Settings",
-              style: TextStyle(
-                fontSize: 50,
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: SenText(
+                "Welcome Hacker!",
+                color: Colors.black87,
+                fontSize: 25.0,
               ),
             ),
-            RaisedButton(
-              child: Text("Log out"),
-              onPressed: () async{
-                await AuthService().logout();
-              },
-            )
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 28.0),
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/ivyhacks_logo.png"),
+                radius: 80,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50.0),
+                    topRight: Radius.circular(50.0),
+                  ),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  children: [
+                    RaisedButton(
+                      child: Text("Log out"),
+                      onPressed: () async {
+                        await AuthService().logout();
+                      },
+                    ),
+                    itemCard("Change Password"),
+                    itemCard("Language"),
+                    itemCard("Preferences"),
+                    itemCard("About"),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
